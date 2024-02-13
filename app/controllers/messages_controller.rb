83e30f78c7cb4fcb15f56e6bc5d0eb196db7d0e4
hamid321 <hamidazim321@gmail.com>
2024-02-13 15:55:53 +0500

    Update messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,6 @@
 class MessagesController < ApplicationController
   def random
     greet = Message.order('RANDOM()').first
-    render json: { greetings: greet }
+    render json: { greetings: greet.text }
   end
 end
